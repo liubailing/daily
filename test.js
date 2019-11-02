59069b45624161a3f660847ca16eaafec99af438
liubailing <bailing.liu@live.com>
2019-11-02 11:08:25 +0800

    测试文件

--- test.js
+++ test.js
@@ -1,34 +1,36 @@
-var tEnum = {
-    a: Symbol(),
-    b: Symbol(),
-    c: Symbol()
-};
-function fn(s) {
-    switch (s) {
-        case tEnum.a:
-            console.log('a');
-            break;
-        case tEnum.b:
-            console.log('b');
-            break;
-        case tEnum.c:
-            console.log('c');
-            break;
-        default:
-            console.log('default');
-            break;
+(function () {
+    try {
+        let iframeNode = document.evaluate(`//IFRAME[@id='g_iframe']`,
+            document.documentElement, null,
+            XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        debugger;
+        if (!iframeNode) return 'error iframe';
+        let str = document.evaluate(`//DIV[@id='song-list-pre-cache']/DIV[1]/DIV[1]/TABLE[1]/TBODY[1]/TR`,
+            iframeNode.contentWindow.document.documentElement,
+            null,
+            XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        console.log('str::', str);
+        return str;
+    } catch (error) {
+        console.error(error);
+        return false;
     }
-}
-fn(tEnum.a);
-fn(tEnum.b);
-fn(tEnum.c);
+})()
 
-{
-    let x = 1
-    function test(y = x) {
-        var x = 3
-        console.log(x)
-    }
 
-    test();
-}
+    (function () {
+        try {
+            let el = document.evaluate(`(//DIV[@id='song-list-pre-cache']/DIV[1]/DIV[1]/TABLE[1]/TBODY[1]/TR)[1]/TD[2]/DIV[1]/DIV[1]/DIV[1]/SPAN[1]/A[1]/B[1]/DIV[1]`,
+                document.documentElement,
+                null,
+                XPathResult.FIRST_ORDERED_NODE_TYPE,
+                null
+            ).singleNodeValue;
+            if (el == null) return '';
+            console.log('str::', el);
+            return el.innerText;
+        } catch (error) {
+            console.error(error);
+            return '';
+        }
+    })()
